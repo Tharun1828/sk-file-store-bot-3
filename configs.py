4bb83eab9deacfa983e0fe9f5da2a149b2824949
Tharun1828 <91731118+Tharun1828@users.noreply.github.com>
2021-12-26 19:39:36 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -26,24 +26,20 @@ Send me any file I will save it in my Database. Also works for channel. Add me t
 
 📝 **Language:** [Python3](https://www.python.org)
 
-📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
+🧑🏻‍💻 **Developer:** @Sk_Media_Official 
 
-📡 **Hosted on:** [Heroku](https://heroku.com)
+👥 **Support Group:** [Jolly Tv](https://t.me/Jolly_Tv)
 
-🧑🏻‍💻 **Developer:** @AbirHasan2005
-
-👥 **Support Group:** [Linux Repositories](https://t.me/DevsZone)
-
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
+📢 **Updates Channel:** [Sk Media Official](https://t.me/Sk_Media_Official)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @Sk_Media_Official 
 
-Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
+Developer is Super Noob. Just Learning from Official Docs. Please Join the developer for Keeping the Service Alive.
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
+[Join](https://t.me/Sk_Media_Official)
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
